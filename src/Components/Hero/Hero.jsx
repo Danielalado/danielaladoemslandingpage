6e5488812b8eca61c64472a4846831eeabdab41c
Danielalado <faithdigitalhub25gmail.com>
2026-06-11 16:49:37 +0000

    Write the responsivness for the home page

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,15 +8,15 @@ export default function Hero () {
     return(
         <section id="hero">
             <div className="hero">
-            <div className="hero-left">
-                <h1>THE</h1>
-                <h1>E-MILLENIAL</h1>
-                <h1>STORE</h1>
-            </div>
+                <div className="hero-left">
+                    <h1>THE</h1>
+                    <h1>E-MILLENIAL</h1>
+                    <h1>STORE</h1>
+                </div>
 
-            <div className="hero-right">
-                <img src={HeroImage} alt="" />
-            </div>
+                <div className="hero-right">
+                    <img src={HeroImage} alt="" />
+                </div>
             </div>
            
 
